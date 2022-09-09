830d25613d33c7f17f635953058a99f8fe7a174e
NicoA8 <nicoastancai@gmail.com>
2022-09-09 16:44:04 +0300

    change

--- src/javascript/bag.js
+++ src/javascript/bag.js
@@ -2,9 +2,11 @@ const addToCartButtons = document.querySelectorAll(".cart-button");
 addToCartButtons.forEach(addToCartButton => {
   const addedBackground = document.querySelector(".added-background");
   const addedBox = document.querySelector(".added-bag-box");
+  const checkSign = document.querySelector(".check");
   addToCartButton.addEventListener("click", () => {
     addedBackground.classList.add("active");
     addedBox.classList.add("active");
+    checkSign.classList.add("active");
     setTimeout(() => {
       addedBackground.classList.remove("active");
       addedBox.classList.remove("active");
