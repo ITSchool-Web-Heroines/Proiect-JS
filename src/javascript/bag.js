8d04f99f30d36e6f2df0374db1947e5494bd003e
NicoA8 <nicoastancai@gmail.com>
2022-09-09 16:31:28 +0300

    Fix

--- src/javascript/bag.js
+++ src/javascript/bag.js
@@ -2,9 +2,12 @@ const addToCartButtons = document.querySelectorAll(".cart-button");
 addToCartButtons.forEach(addToCartButton => {
   const addedBackground = document.querySelector(".added-background");
   const addedBox = document.querySelector(".added-bag-box");
-  addToCartButton.addEventListener("click", () => {});
-  setTimeout(() => {
+  addToCartButton.addEventListener("click", () => {
     addedBackground.classList.add("active");
     addedBox.classList.add("active");
-  }, 200);
+    setTimeout(() => {
+      addedBackground.classList.remove("active");
+      addedBox.classList.remove("active");
+    }, 3000);
+  });
 });
