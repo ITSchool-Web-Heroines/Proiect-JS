1966fcfd459196e68b173fc07c0bf04ed9042d9d
NicoA8 <nicoastancai@gmail.com>
2022-09-11 15:14:52 +0300

    Animation Correction

--- src/javascript/bag.js
+++ src/javascript/bag.js
@@ -4,12 +4,16 @@ addToCartButtons.forEach(addToCartButton => {
   const addedBox = document.querySelector(".added-bag-box");
   const checkSign = document.querySelector(".check");
   addToCartButton.addEventListener("click", () => {
-    addedBackground.classList.add("active");
-    addedBox.classList.add("active");
-    checkSign.classList.add("active");
+    addedBackground.classList.add("active", "fade-in");
+    addedBox.classList.add("active", "fade-in");
+    checkSign.classList.add("active", "fade-in");
     setTimeout(() => {
-      addedBackground.classList.remove("active");
-      addedBox.classList.remove("active");
-    }, 2000);
+      addedBackground.classList.add("fade-out");
+      addedBox.classList.add("fade-out");
+      setTimeout(() => {
+        addedBackground.classList.remove("active", "fade-out");
+        addedBox.classList.remove("active", "fade-out");
+      }, 100);
+    }, 1600);
   });
 });
