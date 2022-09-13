2bd4470d8d50c57f9e44220bd83f9dac876fa55e
NicoA8 <nicoastancai@gmail.com>
2022-09-13 13:59:49 +0300

    title animation

--- src/javascript/newsad.js
+++ src/javascript/newsad.js
@@ -2,12 +2,16 @@
 const btnNewsX = document.querySelector(".news-btn-x");
 const btnNewsSubs = document.querySelector(".ad-news-submit");
 const newsBox = document.querySelector(".news-blur");
+const indexTitle = document.querySelector("p.subtitle.title");
+const indexSubtitle = document.querySelector("p.subtitle.sub-title");
 
 btnNewsX.addEventListener("click", () => {
   newsBox.classList.add("news-fade-out");
   showModal();
   setTimeout(() => {
     newsBox.style.display = "none";
+    indexTitle.classList.add("title-ani");
+    indexSubtitle.classList.add("title-ani");
   }, 150);
 });
 function showModal() {
