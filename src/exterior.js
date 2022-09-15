17c2c264075333235f2620651c48cea0538ed6d5
Belei Bianca <biebianca99@gmail.com>
2022-09-15 09:56:36 +0300

    Update js

--- src/exterior.js
+++ src/exterior.js
@@ -1,18 +1,3 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
-
-document.querySelectorAll(".nav-link, .nav-link-special").forEach((event) =>
-  event.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
-
 const showMore = document.querySelector(".show-hide");
 const text = document.querySelector(".foo");
 showMore.addEventListener("click", (ev) => {
