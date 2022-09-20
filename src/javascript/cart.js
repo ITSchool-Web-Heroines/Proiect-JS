2226af357a060134979cf59260b2e09351228459
NicoA8 <nicoastancai@gmail.com>
2022-09-20 15:53:55 +0300

    Changed game in cart.js, shortened code, same use.

--- src/javascript/cart.js
+++ src/javascript/cart.js
@@ -1,57 +1,24 @@
+const results = document.querySelectorAll(".result-box>div");
 const select = document.querySelector("select");
-const gameResult = document.querySelector(".game-result.search");
-const gameOutcome = document.querySelector(".game-outcome");
-const gameOutcomeBlend = document.querySelector(".blend-title");
-select.addEventListener("change", () => {
+
+function pickBlendOption(blend, index) {
   const choice = select.value;
-  console.log(choice);
-  if (choice === "antigua") {
-    gameResult.src = document.querySelector(".game-result.antigua").src;
-    gameOutcome.style.display = "grid";
-    gameOutcomeBlend.textContent = "ANTIGUA BLEND";
+  if (choice === blend) {
+    results[index].classList.add("active");
   }
-  if (choice === "brazil") {
-    gameResult.src = document.querySelector(".game-result.brazil").src;
-    gameOutcome.style.display = "grid";
-    gameOutcomeBlend.textContent = "BRAZIL BLEND";
-  }
-  if (choice === "decaf") {
-    gameResult.src = document.querySelector(".game-result.decaf").src;
-    gameOutcome.style.display = "grid";
-    gameOutcomeBlend.textContent = "DECAF BLEND";
-  }
-  if (choice === "ethiopia") {
-    gameResult.src = document.querySelector(".game-result.ethiopia").src;
-    gameOutcome.style.display = "grid";
-    gameOutcomeBlend.textContent = "ETHIOPIA BLEND";
-  }
-  if (choice === "french") {
-    gameResult.src = document.querySelector(".game-result.french").src;
-    gameOutcome.style.display = "grid";
-    gameOutcomeBlend.textContent = "FRENCH BLEND";
-  }
-  if (choice === "home") {
-    gameResult.src = document.querySelector(".game-result.home").src;
-    gameOutcome.style.display = "grid";
-    gameOutcomeBlend.textContent = "HOME BLEND";
-  }
-  if (choice === "treat") {
-    gameResult.src = document.querySelector(".game-result.treat").src;
-    gameOutcome.style.display = "grid";
-    gameOutcomeBlend.textContent = "TREAT BLEND";
-  }
-  if (choice === "uganda") {
-    gameResult.src = document.querySelector(".game-result.uganda").src;
-    gameOutcome.style.display = "grid";
-    gameOutcomeBlend.textContent = "UGANDA BLEND";
-  }
-  if (choice === "vanilla") {
-    gameResult.src = document.querySelector(".game-result.vanilla").src;
-    gameOutcome.style.display = "grid";
-    gameOutcomeBlend.textContent = "VANILLA BLEND";
-  }
-  if (choice === "search") {
-    gameResult.src = document.querySelector(".game-search").src;
-    gameOutcome.style.display = "none";
-  }
+}
+select.addEventListener("change", () => {
+  results.forEach(result => {
+    result.classList.remove("active");
+  });
+  pickBlendOption("search", 0);
+  pickBlendOption("antigua", 1);
+  pickBlendOption("brazil", 2);
+  pickBlendOption("decaf", 3);
+  pickBlendOption("ethiopia", 4);
+  pickBlendOption("french", 5);
+  pickBlendOption("house", 6);
+  pickBlendOption("treat", 7);
+  pickBlendOption("uganda", 8);
+  pickBlendOption("vanilla", 9);
 });
